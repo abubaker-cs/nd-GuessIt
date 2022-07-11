a369a5829a83b5c2a3fe608696b4cd1711f15a63
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-07-12 02:20:13 +0500

    Base setup

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -7,4 +7,16 @@ class GameViewModel : ViewModel() {
     init {
         Log.i("GameViewModel", "GameViewModel created!")
     }
+
+    /**
+     * This method will be called when this ViewModel is no longer used and will be destroyed.
+     *
+     *
+     * It is useful when ViewModel observes some data and you need to clear this subscription to
+     * prevent a leak of this ViewModel.
+     */
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("GameViewModel", "GameViewModel destroyed!")
+    }
 }

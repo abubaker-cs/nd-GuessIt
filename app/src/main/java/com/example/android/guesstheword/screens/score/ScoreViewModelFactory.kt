03554741384726cc9c29cb8127d6c7c82c512f47
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-07-12 15:31:50 +0500

    Working with ScoreFragment.kt, ScoreViewModel.kt and ScoreViewModelFactory.kt

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
@@ -5,7 +5,8 @@ import androidx.lifecycle.ViewModelProvider
 
 class ScoreViewModelFactory(private val finalScore: Int) : ViewModelProvider.Factory {
 
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+
         if (modelClass.isAssignableFrom(ScoreViewModel::class.java)) {
             return ScoreViewModel(finalScore) as T
         }
